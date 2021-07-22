e064df2817d4d79d9d80da62d8bcdc2d0ca3fe0b
Guilherme Martins <g.santosmartins@outlook.com>
2021-07-22 15:30:56 -0300

    Codigo refatorado com styled-components

---
+++ src/Components/Cabecalho/index.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import styled from 'styled-components'
+import bank_logo from "../../assets/images/bank_logo.svg";
+
+// Css import from vars
+import {corPrimaria} from '../UI/variaveis'
+
+const StyledHeader = styled.nav`
+    background-color: ${corPrimaria};
+    display: flex;
+    justify-content: space-between;
+    padding: 0 15vw;
+    height: 10vh;
+    align-items: center;
+`
+
+const Logo = styled.img`
+  height: 50px;
+  width: 50px;
+`
+
+const ButtonCabecalho = styled.button`
+  text-align: center;
+  border-radius: 3px;
+  padding: 5px 20px;
+  margin: 0 10px;
+  font-weight: 600;
+  border: 2px solid white;
+
+  background: ${(props)=> props.primary ? "white" : corPrimaria};
+  color:${(props) => props.primary ? corPrimaria : "white"};
+`
+
+
+const Cabecalho = () => {
+  return (
+    <StyledHeader >
+      <Logo className="imagem-logo" src={bank_logo} alt="Logo Smart Bank" />
+      <div>
+        <ButtonCabecalho primary href="https://google.com">
+          Ajuda
+        </ButtonCabecalho>
+        <ButtonCabecalho href="https://google.com">
+          Sair
+        </ButtonCabecalho>
+      </div>
+    </StyledHeader>
+  );
+};
+
+export default Cabecalho;
